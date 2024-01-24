1de6513f17768238209e0b6c5fc411902136144f
mmuchsin <kangucin21@gmail.com>
2024-01-25 06:52:57 +0700

    add users endpoints

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, UserList, UserDetail
 
 urlpatterns = [
     path('', PostList.as_view(), name='post_list'),
     path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+    path('users', UserList.as_view(), name='user_list'),
+    path('users/<int:pk>', UserDetail.as_view(), name='user_detail'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
-from .serializers import PostSerializer
+from django.contrib.auth import get_user_model
+from .serializers import PostSerializer, UserSerializer
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
 
@@ -12,3 +13,13 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
